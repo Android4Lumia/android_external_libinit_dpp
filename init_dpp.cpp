9dd1766ae57c988fad975d0df35ed91ede79d7de
dung11112003 <dung11112003@gmail.com>
2018-04-26 18:19:14 +0700

    Update init_dpp.cpp

--- init_dpp.cpp
+++ init_dpp.cpp
@@ -24,7 +24,6 @@
    OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
    IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
-
 #include <fstream>
 #include <sstream>
 #include <map>
